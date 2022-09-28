f9925af4b56783f9a54f9cd27091d94079276526
bsanchez <bsanchez@106-27.info2>
2022-09-28 10:30:55 +0200

    Segundo commit

---
+++ Ejerciciobasico1/src/ejerciciobasico1/ejerciciobasico1.java
@@ -0,0 +1,13 @@
+package ejerciciobasico1;
+
+public class ejerciciobasico1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("Hello, how are you?");
+        System.out.println("Fine thanks.");
+
+	}
+
+}
